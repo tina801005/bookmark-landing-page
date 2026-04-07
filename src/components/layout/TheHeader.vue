646eb42927378da27915c1e0e1fa546b524336ea
Tina Wang <t870018t@gmail.com>
2026-04-07 11:23:14 +0800

    20260407 - 移除和製作 👉移除reset.css 👉開始製作The.header.vue裡的nav

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -4,21 +4,23 @@
 </script>
 
 <template>
-    <nav class="container flex items-center justify-between bg-amber-500 px-32 py-6">
+    <nav class="debug container flex items-center justify-between py-6 font-base">
         <!-- logo -->
-        <div class="logo">
+        <div class="debug logo flex-1">
             <img src="/src/assets/images/logo-bookmark.svg" alt="bookmark logo" />
         </div>
 
         <!-- menu -->
-        <ul class="menu">
-            <li><a href="#features">Features</a></li>
-            <li><a href="#download">Pricing</a></li>
-            <li><a href="#contact">Contact</a></li>
+        <ul class="debug menu ">
+            <li class="ui-debug button-style  hover:text-red-400"><a href="#features">Features</a></li>
+            <!-- <li class="ui-debug "><a href="#download">Pricing</a></li>
+            <li class="ui-debug "><a href="#contact">Contact</a></li> -->
         </ul>
 
+
+
         <!-- CTA button -->
-        <button class="btn-login">Login</button>
+        <button class="ui-debug btn-login block py-9">Login</button>
     </nav>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import './style.css' //全局樣式
-import './style/reset.css' //樣式重置
 import App from './App.vue'
 
 createApp(App).mount('#app')
