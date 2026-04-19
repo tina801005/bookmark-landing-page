6b7d1f011a3f06138249f13cef2e4e18e48a8a24
Tina Wang <t870018t@gmail.com>
2026-04-19 19:17:52 +0800

    02060419 - HeroSection開始製作。 添加了 1. section、圖片及文字 2. 調整了RWD排版 3. 調整h1在不同視窗界面下的大小及斷行
    下次要做：
    1. button
    2. 圖片背後的藍色圓角(新component)
    3. 刪除所有debug和ui-debug看實際效果

--- src/components/layout/HeroSection.vue
+++ src/components/layout/HeroSection.vue
@@ -3,7 +3,23 @@
 <script setup></script>
 
 <template>
-    <div class="w-20 h-20 bg-amber-500">hero</div>
+    <section class="debug w-full">
+        <div class="ui-debug my-20 flex flex-col items-center lg:flex-row-reverse">
+            <!-- 圖片 -->
+            <div class="w-[clamp(200px,50vw,400px)] mx-auto lg:w-[clamp(300px,40vw,500px)]">
+                <img src="/src/assets/images/illustration-hero.svg" alt="Hero image" class="w-full">
+            </div>
+            <!-- 文字 -->
+            <div class="ui-debug flex flex-col gap-6 p-6 items-center text-center lg:w-1/2 lg:items-start lg:text-left">
+                <h1 className="ui-debug text-[clamp(1.5rem,1rem+3vw,3.5rem)] font-medium text-center lg:text-left max-w-[20ch] leading-tight">A Simple Bookmark Manager</h1>
+                <p class="ui-debug text-center text-very-dark-blue/50 font-medium lg:text-left max-w-md">A clean and simple interface to organize your favorite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
+                <div class="flex items-center justify-center">
+                    <button class="">Get it on Chrome</button>
+                    <button>Get it on Firefox</button>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped></style>
