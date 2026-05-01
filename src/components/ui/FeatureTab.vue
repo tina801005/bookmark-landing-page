330098392c4b1c80fd589a25bff16503b645e940
Tina Wang <t870018t@gmail.com>
2026-05-01 19:04:24 +0800

    20260501 - feature.vue細節微調

--- src/components/ui/FeatureTab.vue
+++ src/components/ui/FeatureTab.vue
@@ -4,10 +4,25 @@
 
 <template>
     <div>
-        <ul class="w-[75%] mx-auto flex flex-col justify-center items-center border-y border-very-dark-blue/20 lg:flex-row lg:border-y-0 lg:border-b">
-            <li class="w-full py-4 grid place-items-center cursor-pointer  lg:border-very-dark-blue/20  hover:text-soft-red"><button class="cursor-pointer">Simple Bookmarking</button></li>
-            <li class="w-full py-4 grid place-items-center cursor-pointer border-y border-very-dark-blue/20 lg:border-y-0  hover:text-soft-red"><button class="cursor-pointer">Speedy Searching</button></li>
-            <li class="w-full py-4 grid place-items-center cursor-pointer  lg:border-very-dark-blue/20  hover:text-soft-red"><button class="cursor-pointer">Easy Sharing</button></li>
+        <ul class="w-[75%] mx-auto flex flex-col justify-center items-center border-y border-very-dark-blue/20
+        lg:flex-row lg:border-y-0 lg:border-b">
+            <li class="w-full grid place-items-center
+             lg:border-very-dark-blue/20">
+                <button class="w-full py-4 cursor-pointer
+                hover:text-soft-red">Simple Bookmarking</button>
+            </li>
+
+            <li class="w-full grid place-items-center border-y border-very-dark-blue/20
+            lg:border-y-0">
+                <button class="w-full py-4 cursor-pointer
+                hover:text-soft-red">Speedy Searching</button>
+            </li>
+
+            <li class="w-full grid place-items-center
+            lg:border-very-dark-blue/20">
+                <button class="w-full py-4 cursor-pointer
+                hover:text-soft-red">Easy Sharing</button>
+            </li>
         </ul>
     </div>
 </template>
